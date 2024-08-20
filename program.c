1f84a1deb814e52f2d8a0082c64d936cdd64e192
Gauri <gauriilakshmii@gmail.com>
2024-08-20 09:36:27 +0530

    Function Enqueue
    Created a function to add elements to the queue

--- program.c
+++ program.c
@@ -1,3 +1,23 @@
 #include<stdio.h>
 //Initialize front, rear, an array queue and its size as global variables
-int front=-1,rear=-1,q[10],size=5;
+int front=-1,rear=-1,q[10],size=5,item;
+
+void Enqueue()
+{
+   if(rear==size-1)
+   {
+      printf("\nQueue is full");
+   }
+   else
+   {
+      printf("\nEnter the element to be inserted :- ");
+      scanf("%d",&item);
+      rear++;
+      q[rear]=item;
+      printf("\nEntered item is :- %d",q[rear]);
+      if(rear==0)
+      {
+        front=0;
+      }
+   }
+}
